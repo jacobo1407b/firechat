7f733223a570bbf088f33107eb386f9d047a6db0
Jacobo Rodrigo Hernandez Mendieta <jacobohernandezmendieta7@gmail.com>
2020-11-08 20:34:01 -0600

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAXqrKP3py6xa1iXfd_68nYXr_V2I6jvr8",
+    authDomain: "firechat-e0375.firebaseapp.com",
+    databaseURL: "https://firechat-e0375.firebaseio.com",
+    projectId: "firechat-e0375",
+    storageBucket: "firechat-e0375.appspot.com",
+    messagingSenderId: "314306805247",
+    appId: "1:314306805247:web:ef1983c2d503d9e2a63bca",
+    measurementId: "G-3S8KDXTFG8"
+  }
+};
